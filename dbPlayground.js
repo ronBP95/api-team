a0c385c9c8bb7b788953b6850d4a8127eb51b456
James Sinkler <jamessinkler713@gmail.com>
2021-03-02 11:10:49 -0800

    remove code, insert instructions

--- dbPlayground.js
+++ dbPlayground.js
@@ -1,61 +1,41 @@
 const db = require('./models')
 
-// Make some functions to play with and use async
 const createProduct = async()=> {
-  const product = await db.Product.create({
-    name:'Cool Kicks',
-    price: 22
-  })
+  // make two products, no Orders yet
 
-  const productTwo = await db.Product.create({
-    name:'Brown Belt',
-    price: 15
-  })
 }
 
 const getProducts = async()=> {
-  const allProducts = await db.Product.find({})
-  console.log(allProducts)
+  // get all Products
 }
 
 // createProduct()
 // getProducts()
 
 const makeAnOrder = async()=> {
-  //get the product with name 'Brown Belt' that already exist and make them part of this new order
-  const productToAssociate = await db.Product.find({ name: 'Brown Belt' })
+  //get a product already created
 
   //associate the foundProduct to a new Order
-  const newOrder = await db.Order.create({
-    products: [productToAssociate[0]]
-  })
-  console.log(newOrder)
 }
 // makeAnOrder()
 
 const makeAnOrderWithAllProducts = async()=> {
-  //get the product with name 'Brown Belt' that already exist and make them part of this new order
+  //get all products
   const products = await db.Product.find({})
 
-  //associate the foundProduct to a new Order
-  const newOrder = await db.Order.create({
-    products: products
-  })
+  //associate the products to a new Order
+
   console.log(newOrder)
 }
-makeAnOrderWithAllProducts()
+//makeAnOrderWithAllProducts()
 
 
 //get one order, see result with/out .populate
 const getOneOrder = async(orderId)=> {
-  const order = await db.Order.findOne({ _id: orderId })
-  console.log('this is the order, ', order)
+  // without .populate('products')
 
   // populate the products field
-  const orderWithPopulate = await db.Order.findOne({  _id: orderId }).populate('products')
-  console.log('this is the order with populate method', orderWithPopulate.products)
 }
-
 // getOneOrder()
 
 
