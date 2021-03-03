2995f6379407b3ff2ec70a9bfdd1f079438337eb
iamniama <plectrumonthespectrum@gmail.com>
2021-03-03 14:45:28 -0700

    getting initial code in

--- controllers/products.js
+++ controllers/products.js
@@ -1,31 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
+router.use(express.urlencoded({extended: false}))
 
-router.get('/', (req, res)=> {
+router.get('/', async(req, res)=> {
   // show all products
-  console.log('all products')
-  res.json({ "all products": "all products" })
+  const allProducts = await db.Product.find({})
+  res.json(allProducts)
 
 })
 
-router.get('/:id', (req, res)=> {
+router.get('/:force_remote', async(req,res)=>{
+  const checkData = await db.Product.count()
+  if (checkData < 1 && req.params.force_remote === "n"){
+    //get data from API
+  }else{
+    //get data from DB
+  }
+})
+
+router.get('/:id', async(req, res)=> {
   // get one product by id
-  console.log('one product')
-  res.json({ "one product": "one product" })
+  const product = await db.Product.findOne({_id: req.params.id})
+  res.json(product)
 })
 
-router.post('/', (req, res)=> {
-  console.log(req.body)
+router.post('/', async(req, res)=> {
+  console.log(req.body.name)
   //create a new product
-  console.log('created product')
-  res.json({ "created": "nothing created yet" })
+  const newProduct = await db.Product.create(req.body)
+  res.json(newProduct)
 })
 
-router.put('/', (req, res)=> {
+router.put('/', async(req, res)=> {
   console.log(req.body)
   //update a product
-  console.log('updated product')
-  res.json({ "updated": "updated" })
+  const updProduct = await db.Product.findOneAndUpdate({_id: req.body.id},
+  {name: req.body.name,
+  price: parseFloat(req.body.price)}, {new: true}
+  )
+  console.log(updProduct)
+  res.json({ updProduct })
+})
+
+router.delete('/', async(req,res)=>{
+  console.log(req.body)
+  const delProduct = await db.Product.findOneandRemove({_id: req.body.id})
+  console.log(delProduct)
+  res.json(delProduct)
 })
 
 module.exports = router
